2cacf087adca9d24fb6a5b46cfa873f3657933e0
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-27 01:39:58 -0400

    ADD: Persiste la data de los temas en el servidor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,66 @@
-import type { AppProps } from "next/app";
+import App, { AppContext, AppInitialProps, AppProps } from "next/app";
+import { getCookie, setCookie, hasCookie } from "cookies-next";
 import { globalCss } from "../stitches.config";
+import PrimaryColorTheme from "../components/PrimaryColor";
+import { TColorMode, TColors } from "../types";
+import ColorModeTheme from "../components/ColorMode";
+import { COLOR_MODE_KEY, PRIMARY_COLOR_KEY } from "../constants";
 
 const globalStyles = globalCss({
   "html, body": {
     margin: 0,
-    backgroundColor: "$slate3",
     fontFamily: "sans-serif",
   },
   "*": {
     boxSizing: "border-box",
     margin: 0,
   },
+  ".app-container": {
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "$background",
+    color: "$text",
+  },
 });
 
-function MyApp({ Component, pageProps }: AppProps) {
+type AppOwnProps = { primaryColor: TColors; colorMode: TColorMode };
+
+function MyApp({
+  Component,
+  pageProps,
+  primaryColor,
+  colorMode,
+}: AppProps & AppOwnProps) {
   globalStyles();
-  return <Component {...pageProps} />;
+
+  return (
+    <ColorModeTheme mode={colorMode}>
+      <PrimaryColorTheme color={primaryColor}>
+        <div className="app-container">
+          <Component {...pageProps} />
+        </div>
+      </PrimaryColorTheme>
+    </ColorModeTheme>
+  );
 }
+
+MyApp.getInitialProps = async (
+  context: AppContext
+): Promise<AppOwnProps & AppInitialProps> => {
+  const ctx = await App.getInitialProps(context);
+  const { req, res } = context.ctx;
+
+  if (!hasCookie(PRIMARY_COLOR_KEY, { req, res })) {
+    setCookie(PRIMARY_COLOR_KEY, "orange", { req, res });
+  }
+
+  return {
+    ...ctx,
+    primaryColor: (getCookie(PRIMARY_COLOR_KEY, { req, res }) ||
+      "orange") as TColors,
+    colorMode: (getCookie(COLOR_MODE_KEY, { req, res }) ||
+      "dark") as TColorMode,
+  };
+};
 
 export default MyApp;
